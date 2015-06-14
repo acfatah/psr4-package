b268662693d173e22ea22890bc3b46dfcbc67111
Achmad F. Ibrahim <acfatah@gmail.com>
2015-06-14 09:20:05 +0800

    Update phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@
  * For full copyright and license information, please see the LICENSE file.
  * Redistributions of files must retain the above copyright notice.
  * 
- * @copyright (c) 2014, {{COPYRIGHT}}
+ * @copyright (c) {{YEAR}}, {{COPYRIGHT}}
  * @link {{HOMEPAGE}}
  * @license http://opensource.org/licenses/mit-license.php The MIT License (MIT)
  */
@@ -21,11 +21,11 @@ $autoload = $basePath . "/vendor/autoload.php";
 
 if (!file_exists($autoload)) {
     die(<<<MSG
-Please run "composer install" to install dependencies and create autoload file.
+ Please run "composer install" to install dependencies and create autoload file.
     
 MSG
     );
 }
 
 $loader = require $autoload;
-$loader->addPsr4('Test\\', 'tests/');
+$loader->addPsr4('Fixture\\', 'tests/Fixture');
