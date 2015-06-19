41dde2917797feb37c9aa987d0f2c2d2ab184b62
Achmad F. Ibrahim <acfatah@gmail.com>
2015-06-19 10:11:06 +0800

    Rename files with metadata

--- install/files.php
+++ install/files.php
@@ -20,7 +20,9 @@ return [
         SOURCE_PATH . DIRECTORY_SEPARATOR . 'tests'     . DIRECTORY_SEPARATOR . 'unit'          . DIRECTORY_SEPARATOR . '.gitkeep'
     ],
     'rename' => [
+        SOURCE_PATH . DIRECTORY_SEPARATOR . '_CHANGELOG.md'     => SOURCE_PATH . DIRECTORY_SEPARATOR . '_CHANGELOG.md',
         SOURCE_PATH . DIRECTORY_SEPARATOR . '_composer.json'    => SOURCE_PATH . DIRECTORY_SEPARATOR . 'composer.json',
+        SOURCE_PATH . DIRECTORY_SEPARATOR . '_LICENSE'          => SOURCE_PATH . DIRECTORY_SEPARATOR . 'LICENSE',
         SOURCE_PATH . DIRECTORY_SEPARATOR . '_nbproject'        => SOURCE_PATH . DIRECTORY_SEPARATOR . 'nbproject',
         SOURCE_PATH . DIRECTORY_SEPARATOR . '_phpunit.xml.dist' => SOURCE_PATH . DIRECTORY_SEPARATOR . 'phpunit.xml.dist',
         SOURCE_PATH . DIRECTORY_SEPARATOR . '_README.md'        => SOURCE_PATH . DIRECTORY_SEPARATOR . 'README.md',
